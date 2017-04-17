502e54ecd081d2bb24bd5b7eacceb2639b04e5db
anush2072 <anush2072@gmail.com>
2017-04-17 07:43:27 -0400

    hangMan Game
    Code to choose a random word from list of words

--- hangmanGame.py
+++ hangmanGame.py
@@ -1,83 +1,101 @@
 #importing the time module
 import time
+import random
 
-#welcoming the user
-name = input("What is your name? ")
+def getRandomWord():
+    listOfWords = [ "wish", "opposite", "illegal", "ski", "yarn", "base", "blue", "squash", "melodic", "grease", "berry", "fuel", "book", 
+                    "feeble", "merciful", "examine", "rampant", "greedy", "smash", "trouble", "knee", "instruct", "remember", "birthday", "digestion" ]
+    # return random.sample(listOfWords, 1)
+    return random.choice(listOfWords);
 
-print("Hello, " + name + "Time to play hangman!")
+def main():
+    #welcoming the user
+    name = input("What is your name? ")
 
-print("")
+    print("Hello, " + name + "! Time to play hangman!\n")
 
-#wait for 1 second
-time.sleep(1)
+    #wait for 1 second
+    time.sleep(1)
 
-print("Start guessing...")
-time.sleep(0.5)
+    print("Start guessing...")
+    time.sleep(0.5)
 
-#here we set the secret
-word = "secret"
+    #here we set the secret
+    word = getRandomWord();
 
-#creates an variable with an empty value
-guesses = ''
+    print(word);
 
-#determine the number of turns
-turns = 10
 
-# Create a while loop
 
-#check if the turns are more than zero
-while turns > 0:         
 
-    # make a counter that starts with zero
-    failed = 0             
 
-    # for every character in secret_word    
-    for char in word:      
 
-    # see if the character is in the players guess
-        if char in guesses:    
+
+
+# #creates an variable with an empty value
+# guesses = ''
+
+# #determine the number of turns
+# turns = 10
+
+# # Create a while loop
+
+# #check if the turns are more than zero
+# while turns > 0:         
+
+#     # make a counter that starts with zero
+#     failed = 0             
+
+#     # for every character in secret_word    
+#     for char in word:      
+
+#     # see if the character is in the players guess
+#         if char in guesses:    
     
-        # print then out the character
-            print (char),    
+#         # print then out the character
+#             print (char),    
 
-        else:
+#         else:
     
-        # if not found, print a dash
-            print ("_"),     
+#         # if not found, print a dash
+#             print ("_"),     
        
-        # and increase the failed counter with one
-            failed += 1    
+#         # and increase the failed counter with one
+#             failed += 1    
 
-    # if failed is equal to zero
+#     # if failed is equal to zero
 
-    # print You Won
-    if failed == 0:        
-        print ("You won")  
+#     # print You Won
+#     if failed == 0:        
+#         print ("You won")  
 
-    # exit the script
-        break              
+#     # exit the script
+#         break              
 
    
-    # ask the user go guess a character
-    guess = input("guess a character:") 
+#     # ask the user go guess a character
+#     guess = input("guess a character:") 
 
-    # set the players guess to guesses
-    guesses += guess                    
+#     # set the players guess to guesses
+#     guesses += guess                    
 
-    # if the guess is not found in the secret word
-    if guess not in word:  
+#     # if the guess is not found in the secret word
+#     if guess not in word:  
  
-     # turns counter decreases with 1 (now 9)
-        turns -= 1        
+#      # turns counter decreases with 1 (now 9)
+#         turns -= 1        
  
-    # print wrong
-        print( "Wrong")
+#     # print wrong
+#         print( "Wrong")
  
-    # how many turns are left
-        print("You have", + turns, 'more guesses')
+#     # how many turns are left
+#         print("You have", + turns, 'more guesses')
  
-    # if the turns are equal to zero
-        if turns == 0:           
+#     # if the turns are equal to zero
+#         if turns == 0:           
     
-        # print "You Loose"
-            print("You Loose")
+#         # print "You Loose"
+#             print("You Loose")
+
+
+if __name__ == "__main__": main()
