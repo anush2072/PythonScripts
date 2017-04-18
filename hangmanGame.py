e194f68feeaddf61082a6f7e48a6060138d1af0f
anush2072 <anush2072@gmail.com>
2017-04-18 07:46:49 -0400

    hangMan Game
    Code to print hanged man on screen
    See if user guessed letter is in the word

--- hangmanGame.py
+++ hangmanGame.py
@@ -1,12 +1,32 @@
 #importing the time module
 import time
 import random
+import sys
 
 def getRandomWord():
     listOfWords = [ "wish", "opposite", "illegal", "ski", "yarn", "base", "blue", "squash", "melodic", "grease", "berry", "fuel", "book", 
                     "feeble", "merciful", "examine", "rampant", "greedy", "smash", "trouble", "knee", "instruct", "remember", "birthday", "digestion" ]
     # return random.sample(listOfWords, 1)
     return random.choice(listOfWords);
+
+def hangAMan(turn):
+
+    if turn == 10:
+         print("-----\n|\n|\n|\n|\n|\n|\n|\n|\n-------\n--------")
+    elif turn == 9:
+        print("-----\n|   |\n|   O\n|\n|\n|\n|\n|\n|\n-------\n--------")
+    elif turn ==  8:
+        print("-----\n|   |\n|   O\n|  -+-\n|\n|\n|\n|\n|\n-------\n--------")
+    elif turn == 7:
+        print("-----\n|   |\n|\n|\n|\n|\n|\n|\n|\n-------\n--------")
+    elif turn == 6:
+        print("-----\n|   |\n|\n|\n|\n|\n|\n|\n|\n-------\n--------")
+    elif turn == 5:
+        print("-----\n|   |\n|\n|\n|\n|\n|\n|\n|\n-------\n--------")
+    elif turn == 4:
+        print("-----\n|   |\n|\n|\n|\n|\n|\n|\n|\n-------\n--------")
+
+
 
 def main():
     #welcoming the user
@@ -17,85 +37,77 @@ def main():
     #wait for 1 second
     time.sleep(1)
 
+    hangAMan(10)
+
     print("Start guessing...")
     time.sleep(0.5)
 
+    #creates an variable with an empty value
+    guesses = ''
+
+    #determine the number of turns
+    turns = 10
+
     #here we set the secret
-    word = getRandomWord();
+    word = getRandomWord()
+    print(word)
 
-    print(word);
+    # Create a while loop
 
+    #check if the turns are more than zero
+    while( turns > 0 ):         
 
+        # ask the user to guess a character
+        guess = input("\nGuess a character:") 
 
+        # set the players guess to guesses
+        guesses += guess                    
+            
+        # make a counter that starts with zero
+        failed = 0 
 
+        # for every character in secret_word    
+        for char in word:      
 
+        # see if the character is in the players guess
+            if char in guesses:    
+                # print then out the character
+                sys.stdout.write (char),    
 
+            else:
+                # if not found, print a dash
+                sys.stdout.write  ("_"),     
+           
+            # and increase the failed counter with one
+                failed += 1    
 
+        # if failed is equal to zero
+        # print You Won
+        if failed == 0:        
+            print ("You won")  
 
-# #creates an variable with an empty value
-# guesses = ''
+        # exit the script
+            break              
 
-# #determine the number of turns
-# turns = 10
-
-# # Create a while loop
-
-# #check if the turns are more than zero
-# while turns > 0:         
-
-#     # make a counter that starts with zero
-#     failed = 0             
-
-#     # for every character in secret_word    
-#     for char in word:      
-
-#     # see if the character is in the players guess
-#         if char in guesses:    
-    
-#         # print then out the character
-#             print (char),    
-
-#         else:
-    
-#         # if not found, print a dash
-#             print ("_"),     
-       
-#         # and increase the failed counter with one
-#             failed += 1    
-
-#     # if failed is equal to zero
-
-#     # print You Won
-#     if failed == 0:        
-#         print ("You won")  
-
-#     # exit the script
-#         break              
-
-   
-#     # ask the user go guess a character
-#     guess = input("guess a character:") 
-
-#     # set the players guess to guesses
-#     guesses += guess                    
-
-#     # if the guess is not found in the secret word
-#     if guess not in word:  
- 
-#      # turns counter decreases with 1 (now 9)
-#         turns -= 1        
- 
-#     # print wrong
-#         print( "Wrong")
- 
-#     # how many turns are left
-#         print("You have", + turns, 'more guesses')
- 
-#     # if the turns are equal to zero
-#         if turns == 0:           
-    
-#         # print "You Loose"
-#             print("You Loose")
+        # if the guess is not found in the secret word
+        if guess not in word:  
+     
+         # turns counter decreases with 1
+            turns -= 1  
+            print(turns)
+            hangAMan(turns)      
+     
+        # print wrong
+            print( "Wrong")
+     
+        # # how many turns are left
+        #     print("You have", + turns, 'more guesses')
+     
+        # # if the turns are equal to zero
+        #     if turns == 0:           
+        
+        #     # print "You Loose"
+        #         print("You Loose")
 
 
 if __name__ == "__main__": main()
